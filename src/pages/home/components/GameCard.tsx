bcc690253f1a7a75a1e06a176f70a022c7d7892f
Bryan Grandon <bryan.grandon.c@gmail.com>
2026-03-31 19:49:41 -0300

    refactor(web):change GameCard

--- src/pages/home/components/GameCard.tsx
+++ src/pages/home/components/GameCard.tsx
@@ -22,15 +22,15 @@ const GameCard = ({ gameKey, front, buttons }: game_card) => {
   const activeOption = selectedGameOptions[gameKey] ?? 'multiple'
 
   return (
-    <section className='rounded-xl p-6 shadow-lg w-full test'>
+    <section className='rounded-xl p-6 shadow-lg w-full border border-gray-500 shadow-gray-500/30'>
       <h3 className='text-2xl font-bold mb-3'>{title}</h3>
       <p className='text-gray-400 mb-4'>{description}</p>
       {optionGame ? (
         <section className='mb-6 space-y-2'>
           <h3>Game Options</h3>
-          <section className='pl-4'>
+          <section className='flex flex-col gap-1 pl-4'>
             {GAME_MODE_OPTIONS.map((option) => (
-              <button key={option.id} className='flex items-center gap-2 p-1 cursor-pointer' onClick={() => updateGameOption({ key: gameKey, value: option.value })}>
+              <button key={option.id} className='flex items-center gap-2 button border border-gray-700 w-full' onClick={() => updateGameOption({ key: gameKey, value: option.value })}>
                 <span className={`w-2 h-2 rounded-full ${activeOption === option.value ? 'bg-blue-500' : 'bg-gray-500'}`}></span>
                 {option.text}
               </button>
@@ -39,10 +39,11 @@ const GameCard = ({ gameKey, front, buttons }: game_card) => {
         </section>
       ) : null}
       <section className='grid grid-cols-2 gap-3'>
-        <button onClick={buttons?.howToPlay} className='border border-gray-600 py-2 rounded-lg hover:bg-gray-700 transition cursor-pointer'>
+        {/* Create a component for buttons */}
+        <button onClick={buttons?.howToPlay} className='button bg-gray-500 text-white hover:bg-gray-600'>
           How to Play
         </button>
-        <button onClick={buttons?.startQuiz} className='bg-blue-600 py-2 rounded-lg font-semibold hover:bg-blue-500 transition cursor-pointer'>
+        <button onClick={buttons?.startQuiz} className='button bg-blue-500 text-white hover:bg-blue-600'>
           Start Quiz
         </button>
       </section>
